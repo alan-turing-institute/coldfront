2970116de47ce7dc6de85aff0dd6361bbde284d2
Iain-S <25081046+Iain-S@users.noreply.github.com>
2021-07-19 11:27:57 +0100

    Add postgres adapter package

--- coldfront/config/database.py
+++ coldfront/config/database.py
@@ -4,22 +4,6 @@ from coldfront.config.env import ENV
 #------------------------------------------------------------------------------
 # Database settings
 #------------------------------------------------------------------------------
-# Set this using the DB_URL env variable. Defaults to sqlite. 
-#
-# Examples:
-#
-# MariaDB:
-#  DB_URL=mysql://user:password@127.0.0.1:3306/database
-#
-# Postgresql:
-#  DB_URL=psql://user:password@127.0.0.1:8458/database
-#------------------------------------------------------------------------------
-DATABASES = {
-    'default': ENV.db_url(
-        var='DB_URL',
-        default='sqlite:///'+os.path.join(os.getcwd(), 'coldfront.db')
-    )
-}
 
 
 #------------------------------------------------------------------------------
@@ -39,16 +23,34 @@ DATABASES = {
 #         'PORT': '',
 #     },
 # }
-#
+
+
 # NOTE: For postgresql you need to: pip install psycopg2
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'coldfront',
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     },
-# }
+if os.environ['DBUSER']:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgres',
+            'USER': os.environ['DBUSER'] + "@coldfront-postgres",
+            'PASSWORD': os.environ['DBPASS'],
+            'HOST': os.environ['DBHOST'] + ".postgres.database.azure.com",
+            'PORT': '5432',
+        },
+    }
+else:
+    # Set this using the DB_URL env variable. Defaults to sqlite. 
+    #
+    # Examples:
+    #
+    # MariaDB:
+    #  DB_URL=mysql://user:password@127.0.0.1:3306/database
+    #
+    # Postgresql:
+    #  DB_URL=psql://user:password@127.0.0.1:8458/database
+    #------------------------------------------------------------------------------
+    DATABASES = {
+        'default': ENV.db_url(
+            var='DB_URL',
+            default='sqlite:///'+os.path.join(os.getcwd(), 'coldfront.db')
+        )
+    }
